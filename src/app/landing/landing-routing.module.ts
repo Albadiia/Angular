edf10ce54751a35be1cd3f5a6fe295d6d3a6566b
Hugo <hugo.lemenorel@gmail.com>
2021-01-29 13:49:09 +0100

    import projet angular

---
+++ src/app/landing/landing-routing.module.ts
@@ -0,0 +1,15 @@
+/* import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { landingComponent } from './landing-module/landing-module.component';
+
+const routes: Routes = [
+    {
+        path: '/', component: landingComponent,
+    }
+];
+
+@NgModule({
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule]
+})
+export class landingRoutingModule { } */
